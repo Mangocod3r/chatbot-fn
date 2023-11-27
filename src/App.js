1ff868731d7253be330d376a56d5a3cdea3b9905
Khiran Gannu <117846037+Mangocod3r@users.noreply.github.com>
2023-11-27 19:06:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
 
-const socket = socketIO.connect('https://chatbot-bn.vercel.app/');
+// const socket = socketIO.connect('https://chatbot-bn.vercel.app/');
+const socket = socketIO.connect("https://chatbot-bn.vercel.app/", {
+   forceNew: true,
+   transports: ["polling"],
+});
+
 
 export default function App() {
   return (
