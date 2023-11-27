e6c0f97f5beaca272cbc3fd28802f03bd548f345
Khiran Gannu <117846037+Mangocod3r@users.noreply.github.com>
2023-11-27 18:47:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
 
-const socket = socketIO.connect('https://chatbot-bn-git-master-mangocod3r.vercel.app/');
+const socket = socketIO.connect('https://chatbot-bn-git-master-mangocod3r.vercel.app');
 
 export default function App() {
   return (
