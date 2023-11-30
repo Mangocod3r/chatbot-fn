f812622169dc730d96a86ed263398524cb014e3d
Khiran Gannu <117846037+Mangocod3r@users.noreply.github.com>
2023-11-30 06:27:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
 
-const socket = socketIO.connect('http://localhost:4000');
+const socket = socketIO.connect("https://chat-bot-bn.onrender.com", {
+   forceNew: true,
+   transports: ['websocket', 'polling'],
+});
 
 export default function App() {
   return (
